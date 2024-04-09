8351feebf6ea2e900068a53cf0c8596d93e2cbd7
LimJunseok <limjunseokdev@gmail.com>
2024-04-09 21:00:04 +0900

    240409

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -6,10 +6,17 @@ export const metadata: Metadata = {
 
 const MyPage: NextPage = () => {
   return (
-    <div className='py-8'>
-      <div className='space-y-1'>
-        <h1 className='text-4xl font-bold'>임준석</h1>
-        <div className='space-x-2 text-gray-500 text-lg'><span>인천공항중학교</span><span>·</span><span>3학년 4반</span></div>
+    <div className='py-8 space-y-12'>
+      <header className='space-y-3'>
+        <div className='space-y-1'>
+          <h1 className='text-4xl font-bold'>임준석</h1>
+          <div className='space-x-2 text-gray-500 text-lg'><span>인천공항중학교</span><span>·</span><span>3학년 4반</span></div>
+        </div>
+        <button className='text-lg bg-cyellow hover:bg-dcyellow px-2 py-1 rounded-md'>정보 수정하기</button>
+      </header>
+      <div className='space-y-5'>
+        <h1 className='font-bold text-3xl'>월간 계획 달성률</h1>
+        <div></div>
       </div>
     </div>
   )
