d9fa41060f25b37d6c5de6548c909a15f438629c
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2024-06-10 19:31:31 +0900

    20240610

---
+++ components/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <h1>로딩중...</h1>
+        </div>
+    )
+}
+
+export default Loading
