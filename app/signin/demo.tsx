5717fb2c791dd1bf59cb9b08b18ee08ad869cd75
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2024-06-13 21:03:10 +0900

    20240613

--- app/signin/demo.tsx
+++ app/signin/demo.tsx
@@ -5,16 +5,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Demo: NextPage = () => {
-    const onClick = async () => {
-      const response = await fetch('/www/users', {
-        method: 'POST',
-        body: JSON.stringify({
-          username: 'test',
-          password: 'ayo'
-        })
-      })
-      console.log(await response.json())
-    }
     return (
         <div className='flex flex-col items-center py-20'>
           <div className='space-y-14 w-1/2'>
@@ -31,7 +21,7 @@ const Demo: NextPage = () => {
                 <p>비밀번호</p>
                 <input type='password' placeholder='비밀번호를 입력하세요.' className={inputCSS} />
               </div>
-              <button onClick={onClick} className='text-lg bg-cyellow rounded w-full py-2 text-center font-medium hover:opacity-70 transition-all'>로그인</button>
+              <button className='text-lg bg-cyellow rounded w-full py-2 text-center font-medium hover:opacity-70 transition-all'>로그인</button>
               <div className='text-base text-center space-x-3 text-gray-500'>
                 <Link href='/signup' className='hover:text-cyellow hover:underline transition-colors'>회원가입</Link>
                 <Link href='/signin/password' className='hover:text-cyellow hover:underline transition-colors'>비밀번호 찾기</Link>
